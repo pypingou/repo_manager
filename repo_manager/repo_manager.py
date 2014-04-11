74a856ad24da01247d507215c2b46626671f5220
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-04-11 14:12:50 +0200

    Add the first work on the repo_manager python module

---
+++ repo_manager/repo_manager.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+import rpm
+import os
+
+TS = rpm.ts()
+TS.setVSFlags(rpm._RPMVSF_NOSIGNATURES)
+
+
+def get_rpm_headers(rpmfile):
+    ''' Open an rpm file and returns the dict containing all its headers
+    information.
+    '''
+    fd = os.open(rpmfile, os.O_RDONLY)
+    headers = TS.hdrFromFdno(fd)
+    os.close(fd)
+    return headers
+
+
+def get_rpm_tag(rpmfile, tag):
+    ''' Return the specified tags from the headers of the specified rpm tag.
+    '''
+    return get_rpm_headers(rpmfile)[tag]
+
+
+def get_rpm_name(rpmfile):
+    ''' Return the name of the rpm according to its headers information.
+    '''
+    return get_rpm_tag(rpmfile, rpm.RPMTAG_NAME)
+
+
+def get_rpm_version(rpmfile):
+    ''' Return the version of the rpm according to its headers information.
+    '''
+    return get_rpm_tag(rpmfile, rpm.RPMTAG_VERSION)
+
+
+def get_rpm_version_release(rpmfile):
+    ''' Return the version-release of the rpm according to its headers
+    information.
+    '''
+    return '%s-%s' % (
+        get_rpm_tag(rpmfile, rpm.RPMTAG_VERSION),
+        get_rpm_tag(rpmfile, rpm.RPMTAG_RELEASE),
+    )
+
+
+def get_duplicated_rpms(folder):
+    ''' Browse all the files in a folder and find out which are RPMs and
+    return the RPMs of an application present multiple time.
+    '''
+    folder = os.path.expanduser(folder)
+
+    seen = {}
+    for filename in os.listdir(folder):
+        if not filename.endswith('.rpm'):
+            continue
+
+        filename = os.path.join(folder, filename)
+        name = get_rpm_name(filename)
+        version = get_rpm_version_release(filename)
+
+        if name in seen:
+            seen[name].append(
+                {'version': version, 'filename': filename}
+            )
+        else:
+            seen[name] = [
+                {'version': version, 'filename': filename}
+            ]
+
+    dups = {}
+    for rpmfile in sorted(seen):
+        if len(seen[rpmfile]) > 1:
+            dups[rpmfile] = seen[rpmfile]
+    return dups
+
+
+def info_repo(folder, keep=3):
+    ''' Returns some info/stats about the specified repo.
+    '''
+    folder = os.path.expanduser(folder)
+
+    print folder
+    cnt_rpm = 0
+    cnt_srpm = 0
+    for filename in os.listdir(folder):
+        if filename.endswith('.src.rpm'):
+            cnt_srpm += 1
+        elif filename.endswith('.rpm'):
+            cnt_rpm += 1
+
+    print '  %s RPMs found' % cnt_rpm
+    print '  %s source RPMs found' % cnt_srpm
+
+    dups = get_duplicated_rpms(folder)
+    cnt = 0
+    for dup in sorted(dups):
+        versions = [rpmfile['version'] for rpmfile in sorted(dups[dup])]
+        keep_versions = versions[-keep:]
+        for rpmfile in sorted(dups[dup]):
+            if rpmfile['version'] not in keep_versions:
+                cnt += 1
+
+    print '  %s SRPMs/RPMs could be removed' % cnt
