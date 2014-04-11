70cf705535418b207c2762a6770a9696aba17d3b
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-04-11 14:13:09 +0200

    Add __init__ of the repo_manager module with the ``info`` command working

---
+++ repo_manager/__init__.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+import ConfigParser
+import argparse
+import logging
+
+import repo_manager
+
+
+__version__ = '0.1.0'
+logging.basicConfig()
+LOG = logging.getLogger("repo-manager")
+
+
+def do_info(args):
+    ''' Return information about a repo. '''
+    LOG.info("Info")
+    LOG.info("repos   : {0}".format(args.repos))
+    LOG.info("config  : {0}".format(args.configfile))
+    for repo in args.repos:
+        repo_manager.info_repo(repo)
+
+
+def do_add(args):
+    ''' Add a rpm to a repository. '''
+    LOG.info("Add")
+    LOG.info("rpms    : {0}".format(args.rpms))
+    LOG.info("repo    : {0}".format(args.repo))
+    LOG.info("config  : {0}".format(args.configfile))
+
+
+def setup_parser():
+    '''
+    Set the main arguments.
+    '''
+    parser = argparse.ArgumentParser(prog="repo-manager")
+    # General connection options
+    parser.add_argument(
+        '--config', dest="configfile",
+        help="Configuration file to use instead of the default one in "
+        "~/.config/repo_manager")
+    parser.add_argument(
+        '-v', '--verbose', action='store_true',
+        help="Gives more info about what's going on")
+    parser.add_argument(
+        '--debug', action='store_true',
+        help="Outputs bunches of debugging info")
+    parser.add_argument(
+        '--version', action='version',
+        version='repo-manager %s' % (__version__))
+
+    subparsers = parser.add_subparsers(title='actions')
+
+    ## INFO
+    parser_acl = subparsers.add_parser(
+        'info',
+        help='Provides information / stats about a repo')
+    parser_acl.add_argument(
+        'repos', default=None, nargs="*",
+        help="Repositories to investigate")
+    parser_acl.add_argument(
+        '--keep', default=3,
+        help="Number of RPMs of an application to keep")
+    parser_acl.set_defaults(func=do_info)
+
+    ## ADD
+    parser_acl = subparsers.add_parser(
+        'add',
+        help='Add one or more RPMs into a repository')
+    parser_acl.add_argument(
+        'rpms', default=None, nargs="+",
+        help="RPMs to add")
+    parser_acl.add_argument(
+        '--repo', default=None, nargs="?",
+        help="Repository to add the RPMs to")
+    parser_acl.set_defaults(func=do_add)
+
+    return parser
+
+
+def main():
+    ''' Main function of the repo_manager project.
+    '''
+    # Set up parser for global args
+    parser = setup_parser()
+    # Parse the commandline
+    try:
+        arg = parser.parse_args()
+    except argparse.ArgumentTypeError, err:
+        print "\nError: {0}".format(err)
+        return 2
+
+    if arg.debug:
+        LOG.setLevel(logging.DEBUG)
+    elif arg.verbose:
+        LOG.setLevel(logging.INFO)
+
+    return_code = 0
+
+    try:
+        arg.func(arg)
+    except KeyboardInterrupt:
+        print "\nInterrupted by user."
+        return_code = 1
+
+    return return_code
+
+
+if __name__ == '__main__':
+    main()
