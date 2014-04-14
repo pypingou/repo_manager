5a01734a17699f123e81ca41d211ddd58c61f7a6
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-04-14 16:05:04 +0200

    Add the setup.py file

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+Setup script
+"""
+
+from distutils.core import setup
+from repo_manager import __version__
+
+setup(
+    name='repo_manager',
+    description='A simple application to manage RPM repositories',
+    version=__version__,
+    author='Pierre-Yves Chibon',
+    author_email='pingou@pingoured.fr',
+    maintainer='Pierre-Yves Chibon',
+    maintainer_email='pingou@pingoured.fr',
+    license='GPLv3+',
+    url='https://github.com/pypingou/repo_manager',
+    entry_points="""
+    [console_scripts]
+        repo_manager = repo_manager:main
+    """,
+    packages=['repo_manager'],
+)
